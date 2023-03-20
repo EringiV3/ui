e0e339eeec99d48734b46b7a5bc4b72df2d0892f
kodai hagihara <39426616+EringiV3@users.noreply.github.com>
2023-03-21 01:43:37 +0900

    add storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+  title: 'Example/Button',
+  component: Button,
+};
+export default meta;
+
+export const Primary: StoryObj<typeof Button> = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
